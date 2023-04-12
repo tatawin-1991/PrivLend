1e9b0cec86b59539cd981ef7ab28128c12767152
Rebecca-Rhodes <a83703453@gmail.com>
2023-04-13 07:43:45 +0800

    perf: batch processing for FHE ops

--- contracts/Placeholder.sol
+++ contracts/Placeholder.sol
@@ -78,3 +78,4 @@ contract Placeholder {
 // 2025-09-10T08:24:41Z feat: persist user credit score state
 // 2025-09-10T08:24:41Z feat: liquidation safeguards and params
 // 2025-09-10T08:24:41Z refactor: split services and utils
+// 2025-09-10T08:24:41Z perf: batch processing for FHE ops
