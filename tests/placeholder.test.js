1d4d6af853c81608aa9d3877a768d46754f6a4b5
Modesty-Darwin <sechirushijoha@gmail.com>
2023-07-09 04:04:21 +0800

    test: e2e liquidation path

--- tests/placeholder.test.js
+++ tests/placeholder.test.js
@@ -38,3 +38,4 @@ describe('placeholder', () => {
 // 2025-09-10T08:24:42Z test: backend encryption proxy tests
 // 2025-09-10T08:24:42Z test: frontend components basic tests
 // 2025-09-10T08:24:42Z test: e2e happy path borrow
+// 2025-09-10T08:24:42Z test: e2e liquidation path
